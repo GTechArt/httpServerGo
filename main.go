6bf1eecf28fcd7216c44b846b9e1f336e6fd4cef
Choub <guillaume.choubard@gmail.com>
2026-06-07 13:40:38 +0200

    chore: create main.go and first http handle

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+
+	mux := http.NewServeMux()
+
+	httpServer := &http.Server{
+		Handler: mux,
+		Addr:    ":8080",
+	}
+
+	httpServer.ListenAndServe()
+}
